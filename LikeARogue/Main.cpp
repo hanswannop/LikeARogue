36d18a1a59bd20fb95326c0ce77eca8b6ca584ef
Hans Wannop <hanswannop@me.com>
2019-09-05 14:50:32 +0200

    Update include paths. Color change.

--- LikeARogue/Main.cpp
+++ LikeARogue/Main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 	SDL_Window* window = SDL_CreateWindow("LikeARogue", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 127, 0, 127, 255);
 
 	SDL_RenderClear(renderer);
 
